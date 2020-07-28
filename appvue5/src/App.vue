9e5c75bb86f73b6460b3bb39a401c78f4a24daf8
M3ANMAXINE <CHERRERAJ@HOTMAIL.COM>
2020-07-27 22:46:23 -0400

    creacion trabajo 5 props

---
+++ appvue5/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+       
+    <H2>TAREAS</H2>
+
+    <input type="text" v-model="valor" class="form-control">
+    <button class="btn btn-primary" v-on:click="crearTarea()">Crear</button>
+
+    <tareas :lista="lista"    @id="eliminarx" />
+  </div>
+</template>
+
+<script>
+import tareas from './components/tarefas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    tareas,
+  },
+  data() {
+    return {
+      lista: [],
+      valor: "",
+         };
+   },
+    methods: {
+    crearTarea() {
+      if(this.valor !=="")  
+    this.lista.push(this.valor);
+    this.valor = "";
+    },
+            
+    eliminarx(index) {
+
+      this.lista.splice(index, 1);
+     
+    }
+   }}
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ appvue5/src/components/tarefas.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    
+
+
+   
+
+
+    <ul v-for="(tarea,index) in lista" :key="index"> 
+
+        <li > {{ index }} {{' ' + tarea }}     <button class="btn btn-primary" v-on:click="eliminar(index)">Eliminar</button></li>        
+    </ul>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: "tareas",
+  
+         props: {
+
+          lista: Array,    
+         },
+         methods: {
+
+           eliminar(index){
+             console.log(index)
+
+             this.$emit('id', index);
+
+           }
+         }
+  
+};
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
